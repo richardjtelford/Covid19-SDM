11192c958778ea8f613808402ac69aad60dedb95
richardjtelford <richard.telford@bio.uib.no>
2020-03-18 09:52:01 +0100

    recoded for new data import

--- R/case_map.R
+++ R/case_map.R
@@ -2,9 +2,14 @@ library("tidyverse")
 
 #download data
 source("R/download_data.R")
+
+cases <- DownloadData()
   
 cases_to_map <- cases %>% 
-  select(`Province/State`, `Country/Region`, Lat, Long, march10 = `3/10/20`, latest = last_col()) %>% 
+  filter(date == "2020-03-10" | date == max(date)) %>% 
+  mutate(when = if_else(date == "2020-03-10", "march10", "latest")) %>%
+  select(-date) %>% 
+  pivot_wider(names_from = when, values_from = cases) %>% 
   mutate(five = case_when(
     march10 >= 5 ~ "by 10 March",
     latest >= 5 ~ "by now", 
