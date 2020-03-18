374e8eb3efa0fccf3a7f900c33843e0d71617048
richardjtelford <richard.telford@bio.uib.no>
2020-03-18 13:04:31 +0100

    plots

--- R/chelsa_temp.R
+++ R/chelsa_temp.R
@@ -1,33 +1,74 @@
 library(tidyverse)
 
+cases <- DownloadData()
+
 #data from 
 #https://envidatrepo.wsl.ch/uploads/chelsa/chelsa_V1/climatologies/temp/integer/temp/
 temp_feb <- raster::raster("data/CHELSA_temp10_02_1979-2013_V1.2_land.tif")
 
-loc <- cases %>% select(Lat, Long)
-sp::coordinates(loc) <- ~ Long + Lat
+#correct location of French Polynesia (should be corrected in source soon - issue #641)
+cases <- cases %>% 
+  mutate(Long = case_when(
+    `Province/State` == "French Polynesia" & Long > 0 ~ -Long, 
+    TRUE ~ Long))
+
+#make spatial points df of cases (should use sf but cannot install)
+loc <- cases %>%
+  select(Lat, Long) %>% 
+  distinct() %>% 
+  ungroup()
+loc_sp <- loc %>% 
+  select(Lat, Long)
+sp::coordinates(loc_sp) <- ~ Long + Lat
  
-cases_temp <- cases %>% 
-  mutate(feb = raster::extract(temp_feb, loc), #spot temp, fast
+#extract temperatures
+loc_temp <- loc %>% 
+  mutate(feb = raster::extract(temp_feb, loc_sp)) #spot temp, fast
         
-         feb = feb/10)
+#with buffer for sites in ocean 40km is minumum that gets all (Bahamas is worst)
+loc_sp2 <- loc_sp[is.na(loc_temp$feb)]
 
+loc_temp2 <- loc_temp %>% 
+  filter(is.na(feb)) %>%
+  select(-feb) %>% 
+  mutate(feb_buffered = raster::extract(temp_feb, loc_sp2, buffer = 40000, fun = mean, na.rm = TRUE))
 
-feb = ifelse(
-  test = is.na(feb), 
-  yes = raster::extract(temp_feb, loc, buffer = 1000, fun = mean, na.rm = TRUE),
-  no = feb),#try to catch missing data with buffer
+loc_temp2
 
+#combine spot and buffered data
+loc_temp <- left_join(loc_temp, loc_temp2) %>% 
+  mutate(feb = coalesce(feb, feb_buffered)) %>% 
+  select(-feb_buffered) %>% 
+  mutate(feb = feb/10)
+
+#map
 mp <- map_data("world")
-temp_map <- ggplot(cases_temp %>% filter(Long > 150), aes(x = Long, y = Lat, colour = is.na(feb))) +
+temp_map <- ggplot(loc_temp, aes(x = Long, y = Lat, colour = feb)) +
   geom_map(map = mp, data = mp, aes(map_id = region), inherit.aes = FALSE, fill = "grey80", colour = "grey40") +
   geom_point() +
   coord_quickmap() +
-#  scale_colour_viridis_c() +
+  scale_colour_viridis_c() +
   theme(legend.position = "bottom",
         axis.title = element_blank())
 temp_map
 
-cases_temp %>% filter(is.na(feb)) %>% 
-  select(1:4, last_col(1)) %>% 
-  view
+#join with cases
+cases_temp <- cases %>% left_join(loc_temp) %>% 
+  filter(date == "2020-03-10" | date == max(date)) 
+
+#plot cases by temperature
+cases_temp %>% 
+  ggplot(aes(x = feb, y = log(cases), colour = as.factor(date))) +
+  geom_smooth() + 
+  geom_point()
+
+#plot change in cases by temperature
+cases_temp %>% 
+  pivot_wider(names_from = date, values_from = cases) %>% 
+  rename(march10 = last_col(1), latest = last_col()) %>% 
+  ggplot(aes(x = feb, y = log1p(latest) - log1p(march10), colour = log10(march10))) +
+  geom_point() +
+  scale_colour_viridis_c() 
+
+
+
