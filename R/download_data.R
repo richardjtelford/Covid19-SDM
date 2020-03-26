c05f221e23b8e60dc91c1c7b2699dd4dbd041d08
richardjtelford <richard.telford@bio.uib.no>
2020-03-26 11:21:52 +0100

    updated url

--- R/download_data.R
+++ R/download_data.R
@@ -1,7 +1,8 @@
 library("tidyverse")
 
+                               
 #download cases data
-DownloadData <- function(file="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") {
+DownloadData <- function(file="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv") {
   cases <- read_csv(file)
   
   #pivot
